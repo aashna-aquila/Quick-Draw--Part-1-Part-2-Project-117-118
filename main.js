0d9bd1bfeb8b058e5cf49b06335c33f3d5656a57
aashna-aquila <86475797+aashna-aquila@users.noreply.github.com>
2021-10-26 06:32:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function setup()
+{
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+}
+function draw()
+{
+    check_sketch();
+    
+}
+
+
+
+function clear()
+{
+ background("white");
+}
